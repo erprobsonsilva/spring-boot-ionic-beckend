05dff23c04756e486fd454a3353665f25ddd3e37
Robson da Silva <erp.robson.silva@gmail.com>
2018-07-23 14:58:45 -0300

    Produto e Associacao muitos para muitos

--- src/main/java/com/robsonsilva/cursomc/domain/Categoria.java
+++ src/main/java/com/robsonsilva/cursomc/domain/Categoria.java
@@ -1,11 +1,16 @@
 package com.robsonsilva.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import com.robsonsilva.cursomc.Produto;
 
 @Entity
 public class Categoria implements Serializable {
@@ -15,6 +20,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String Nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {	
 	}
@@ -39,6 +47,14 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.Nome = nome;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
