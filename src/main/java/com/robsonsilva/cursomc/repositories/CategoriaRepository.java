5e0f0aa85d8013fdb043ad2c07df7c15a2c54b1b
Robson da Silva <erp.robson.silva@gmail.com>
2018-07-23 14:14:27 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/robsonsilva/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.robsonsilva.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.robsonsilva.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
